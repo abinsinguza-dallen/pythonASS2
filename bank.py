6778bc289121be402203454e473183bbce04685d
-abinsinguza-dallen <dallenabinsinguza@gmail.com>
2022-05-27 22:04:52 +0300

    first commit

---
+++ bank.py
@@ -0,0 +1,15 @@
+class Account:
+    bank="PostBank"
+    def __init__(self,account_number,account_name,balance):
+        self.account_name=account_name
+        self.account_number=account_number
+        self.balance=balance
+    def withdraw(self,amount):
+        self.amount=amount  
+        self.balance-=self.amount
+        return self.balance  
+
+    def deposit(self,amount):
+        self.amount=amount
+        self.balance+=self.amount
+        return self.balance   
